a385e895f45974848be99a2b2a33975fc46d3e61
hieunguyenminh <hieu.mnguyen@katalon.com>
2025-04-07 18:53:21 +0700

    update

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [htmlContent, setHtmlContent] = useState<string>('');
+
+  useEffect(() => {
+    // Fetch the HTML file from the public folder
+    fetch('/example.html')
+      .then((response) => response.text())
+      .then((data) => setHtmlContent(data))
+      .catch((error) => console.error('Error loading HTML file:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            My sample React app
-          </p>
-        </div>
+        <div
+          style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+          dangerouslySetInnerHTML={{ __html: htmlContent }}
+        />
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; 
